af90204fe71bc67fafddbb1e1ebad02cdb5ed004
manish <manish.sep1989@gmail.com>
2013-07-21 22:38:29 +0530

    Modified web.js

--- web.js
+++ web.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var app = express();
+var fs = require("fs");
+//var buffer= require("buffer");
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+    fs.readFileSync("/home/Songs_and_Docs/Study/coursera/startup/bitstarter/index.html",function(err,data){
+	if(err){
+	   throw err;
+	}else{
+	   var buffer = new Buffer(data);
+	}
+    });
+  response.send(buffer.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
